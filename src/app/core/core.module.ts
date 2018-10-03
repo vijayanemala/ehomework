218531e4cc1046b80e8ae0bee539bf5ffd1f9e27
vijayanemala <vijaya@olivetech.net>
2018-10-03 17:23:58 +0530

    designed home page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent }     from './home/home.component';
+import { HeaderComponent }   from './header/header.component';
+import { FooterComponent }   from './footer/footer.component';
+import { LoginComponent }    from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreRoutingModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, HeaderComponent, FooterComponent, LoginComponent]
 })
 export class CoreModule { }
